4a9eb3d98f885da44b9a03fb2fb5f07c19d2b5ed
vincephung <vincentphung43@yahoo.com>
2020-09-29 21:00:20 -0400

    Updates

---
+++ precache-manifest.7f4c8049e3b997d3dd6d86b67ee9cf72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460a5c32126cf2a67119d780c1bd3310",
+    "url": "/photo-tag/index.html"
+  },
+  {
+    "revision": "ba44d0c42c1cf95d3e52",
+    "url": "/photo-tag/static/css/main.4db0dc79.chunk.css"
+  },
+  {
+    "revision": "1b64e0b1755c6d28ff83",
+    "url": "/photo-tag/static/js/2.8925b07d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/photo-tag/static/js/2.8925b07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba44d0c42c1cf95d3e52",
+    "url": "/photo-tag/static/js/main.5758ca61.chunk.js"
+  },
+  {
+    "revision": "6fa896896e5b907d11ae",
+    "url": "/photo-tag/static/js/runtime-main.9f99b6bc.js"
+  },
+  {
+    "revision": "adf17479eabfee8b890b42e7c6211f8a",
+    "url": "/photo-tag/static/media/alice.adf17479.png"
+  },
+  {
+    "revision": "1f526c92ad0647fd0b6fae9ee556bdf2",
+    "url": "/photo-tag/static/media/billy.1f526c92.png"
+  },
+  {
+    "revision": "b6641c0f9aeb595154bcce8fc72f4125",
+    "url": "/photo-tag/static/media/img.b6641c0f.jpg"
+  },
+  {
+    "revision": "eedceb8327f492f7a989cfacfe58fa7f",
+    "url": "/photo-tag/static/media/joe.eedceb83.png"
+  }
+]);
